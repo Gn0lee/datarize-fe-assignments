99bb66837b32f7231cc22c284b38c3d88b432ea2
Gn0lee <jinho605@gmail.com>
2024-08-15 16:18:50 +0900

    fix: 쿼리 옵션 파라미터 타입 수정

--- apps/frontend/src/queries/option.ts
+++ apps/frontend/src/queries/option.ts
@@ -2,12 +2,13 @@ import { queryOptions } from '@tanstack/react-query'
 
 import apiInstance from 'src/lib/ky'
 import type { PurchaseFrequency } from 'src/types/data'
+import type { PurchaseFrequencyRange } from 'src/store/purchase-frequency/atom'
 
-export const purchaseFrequencyQueryOptions = (searchParams?: { from: string; to: string }) =>
+export const purchaseFrequencyQueryOptions = ({ from, to }: PurchaseFrequencyRange) =>
   queryOptions({
-    queryKey: ['purchaseFrequency', searchParams?.from, searchParams?.to],
+    queryKey: ['purchaseFrequency', from, to],
     queryFn: async () =>
-      apiInstance.get('purchase-frequency', { searchParams: searchParams }).json<PurchaseFrequency[]>(),
+      apiInstance.get('purchase-frequency', { searchParams: { from, to } }).json<PurchaseFrequency[]>(),
     select: (data) => data,
     retry: 1,
   })
