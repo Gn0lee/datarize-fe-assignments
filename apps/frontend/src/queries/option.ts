bf86cc7a4050892c9e1953d9ffbf8fbb6bf5dd6e
Gn0lee <jinho605@gmail.com>
2024-08-15 16:52:07 +0900

    feat: api 데이터 가공 함수 추가

--- apps/frontend/src/queries/option.ts
+++ apps/frontend/src/queries/option.ts
@@ -1,14 +1,15 @@
 import { queryOptions } from '@tanstack/react-query'
 
-import apiInstance from 'src/lib/ky'
+import apiInstance from 'src/lib/common/ky'
 import type { PurchaseFrequency } from 'src/types/data'
 import type { PurchaseFrequencyRange } from 'src/store/purchase-frequency/atom'
+import { convertPurchaseFrequencyDataToKRW } from 'src/lib/purchase-frequency/chart'
 
 export const purchaseFrequencyQueryOptions = ({ from, to }: PurchaseFrequencyRange) =>
   queryOptions({
     queryKey: ['purchaseFrequency', from, to],
     queryFn: async () =>
       apiInstance.get('purchase-frequency', { searchParams: { from, to } }).json<PurchaseFrequency[]>(),
-    select: (data) => data,
+    select: (data) => convertPurchaseFrequencyDataToKRW(data),
     retry: 1,
   })
